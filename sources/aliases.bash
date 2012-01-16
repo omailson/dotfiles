114e7b1d1c167d01dc9575f6d96ef1c89408acd4
Mailson Lira <mailson@gmail.com>
2012-01-15 22:00:02 -0300

    Add aliases
    Signed-off-by: Mailson Lira <mailson@gmail.com>

---
+++ sources/aliases.bash
@@ -0,0 +1,16 @@
+alias ls='ls --color=auto'
+alias vi='vim'
+alias go='kde-open'
+alias agora='date +"%d/%m %H:%M"'
+alias genius='genius --nomixed' # Inicia o genius de forma que não mostre frações de forma mista
+alias cakepath='export PATH=$PATH:`pwd`/cake/console'
+alias python='python2'
+alias xx='chmod +x'
+# alias ruby='ruby-1.8'
+
+# Git aliases
+alias k='gitk --all'
+alias ks='git status'
+alias kf='git diff'
+alias t='tig --all'
+alias ts='tig status'
