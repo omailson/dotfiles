7c5ebb1178f0661eea88f883eed6ef86ed224e65
Mailson Menezes <mailson.menezes@openbossa.org>
2012-01-16 16:09:51 -0300

    Improve gotofolder to accept local alias
    Signed-off-by: Mailson Menezes <mailson.menezes@openbossa.org>

--- sources/gotofolder.bash
+++ sources/gotofolder.bash
@@ -1,12 +1,31 @@
 # goto folder - a bash script created by Mailson Lira
 #
-# CD to a folder faster. Just add the aliases to $GOTOFOLDERS variable
+# Change folders faster. Just add the aliases to $GOTOFOLDERS variable
+# alias:path,anotheralias:anotherpath
+#
 # Example: 
 # export GOTOFOLDERS=small:/home/mailson/long/path,$GOTOFOLDERS
 # Then type 'goto small' and you'll be there
+#
+# You can also have a .goto on any folder to write local aliases
+#
+# Example:
+# echo "alias:path/to/local/folder" >> .goto
 
 function goto() {
-	for folder in $(echo $GOTOFOLDERS | tr "," " ")
+	folders=""
+	if [ -e .goto -a -e "$HOME"/.goto ]
+	then
+		folders=$(cat .goto "$HOME"/.goto)
+	elif [ -e .goto ]
+	then
+		folders=$(cat .goto)
+	elif [ -e "$HOME"/.goto ]
+	then
+		folders=$(cat "$HOME"/.goto)
+	fi
+
+	for folder in $folders
 	do
 		foldername=$(echo $folder | cut -d : -f 1)
 		folderpath=$(echo $folder | cut -d : -f 2)
