2af6620daf57653099b54a0d41c3d0721dafb904
Mailson Lira <mailson@gmail.com>
2012-04-07 18:48:42 -0300

    [gotofolder] Parse .goto files recursively
    Now the bookmarks defined on a .goto file will be available to all
    children directories.
    
    Signed-off-by: Mailson Lira <mailson@gmail.com>

--- sources/gotofolder.bash
+++ sources/gotofolder.bash
@@ -12,18 +12,23 @@
 # Example:
 # echo "alias:path/to/local/folder" >> .goto
 
-function goto() {
-	folders=$(echo $GOTOFOLDERS | tr "," " ")
-	if [ -e .goto -a -e "$HOME"/.goto ]
-	then
-		folders=$(cat .goto "$HOME"/.goto)
-	elif [ -e .goto ]
-	then
-		folders=$(cat .goto)
-	elif [ -e "$HOME"/.goto ]
-	then
-		folders=$(cat "$HOME"/.goto)
-	fi
+goto() {
+	local folders foldername folderpath gotopath
+	folders=$(echo $GOTOFOLDERS | tr "," '\n')
+
+	gotopath="$PWD"
+	while [ 1 ]
+	do
+		if [ -e "$gotopath"/.goto ]
+		then
+			folders="$folders"$'\n'$(cat "$gotopath"/.goto)
+		fi
+
+		test "$gotopath" = $HOME && break
+
+		gotopath=$(readlink -f "$gotopath"/..)
+	done
+
 
 	for folder in $folders
 	do
@@ -38,3 +43,5 @@ function goto() {
 		fi
 	done
 }
+
+export -f goto
