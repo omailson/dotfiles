82ed23ff31f97d2e7e2f8acf2a53203364a054f9
Mailson Lira <mailson@gmail.com>
2014-01-05 21:53:36 -0300

    gitbackup: Add untracked files as well
    Untracked files are also important and should be backed up, however some
    untracked files may not be available publicly, that's why we need to ask
    the user if we should backup those files too.
    
    Signed-off-by: Mailson Lira <mailson@gmail.com>

--- bin/gitbackup.sh
+++ bin/gitbackup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # Script to perform a remote backup of the current git workspace
 
+gitbackup_untracked_files=$(git ls-files --others --directory)
+for f in $gitbackup_untracked_files
+do
+	read -p "Add untracked file? $f [Y/n] "
+	if [ "$REPLY" = "y" -o -z "$REPLY" ]
+	then
+		git add $f
+	fi
+done
+unset gitbackup_untracked_files
+
 git commit -a -m WIP
 git push -f origin HEAD:backup-"$USER"
 git reset HEAD~1
