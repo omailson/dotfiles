1c59e27366af980bc7b986c57c593f3e4de645fb
Mailson Lira <mailson@gmail.com>
2015-07-20 21:26:58 -0300

    Offer user to install dotfiles
    The user just need to hit 'y' when asked and configure.sh will add the
    correspondent line to .bashrc

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CONF_STATUS=0
+INSTALL_COMMAND="source $PWD/dotfilesrc"
 
 # Initial check
 if [ ! -f dotfilesrc ]
@@ -15,11 +16,21 @@ then
 	CONF_STATUS=1
 fi
 
+# yes/no function
+source sources/ask.sh
+
 if [ -z "$DOTFILES_PATH" ]
 then
-	echo "Please, add the following line to your main ~/.bashrc"
-	echo "source `pwd`/dotfilesrc"
-	CONF_STATUS=1
+	echo "You should add the following line to your main ~/.bashrc"
+	echo "$INSTALL_COMMAND"
+	if ask "Do you want me to add it for you?"
+	then
+		echo "" >> $HOME/.bashrc
+		echo "# My dotfiles" >> $HOME/.bashrc
+		echo "$INSTALL_COMMAND" >> $HOME/.bashrc
+	else
+		CONF_STATUS=1
+	fi
 fi
 
 # Configure
@@ -32,5 +43,11 @@ fi
 
 echo "Updating submodules..."
 git submodule update --init
+
+if [ "$CONF_STATUS" -eq 0 -a -z "$DOTFILES_PATH" ]
+then
+	echo "Configuration ended successfully. Type the following command to start using now:"
+	echo "$INSTALL_COMMAND"
+fi
 
 exit "$CONF_STATUS"
---
+++ sources/ask.sh
@@ -0,0 +1,35 @@
+# This is a general-purpose function to ask Yes/No questions in Bash, either
+# with or without a default answer. It keeps repeating the question until it
+# gets a valid answer.
+
+ask() {
+    # http://djm.me/ask
+    while true; do
+
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+
+        # Ask the question - use /dev/tty in case stdin is redirected from somewhere else
+        read -p "$1 [$prompt] " REPLY </dev/tty
+
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
