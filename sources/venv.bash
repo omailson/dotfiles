979c0a0520aaa6e1cab4cae81f86e1ee60ab5fa5
Mailson Menezes <mailson@gmail.com>
2019-08-14 23:51:09 -0300

    Customize how virtualenv information is displayed in PS1

---
+++ sources/venv.bash
@@ -0,0 +1,15 @@
+export VIRTUAL_ENV_DISABLE_PROMPT="yes"
+
+__venv_name() {
+	local project_name
+
+	# Only replace the (env) in PS1 if we disable virtualenv from doing so
+	# Also, check whether the venv is activated
+	if [ -n "$VIRTUAL_ENV_DISABLE_PROMPT" -a -n "$VIRTUAL_ENV" ]
+	then
+		project_name=$(basename $(dirname "$VIRTUAL_ENV"))
+		printf -- "$1" "venv:$project_name"
+	else
+		printf -- ""
+	fi
+}
