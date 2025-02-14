5e8e52367aff0e57441ad151b2adad04ca96302f
Mailson Menezes <mailson@gmail.com>
2025-02-14 20:36:29 +0000

    Add modules source to zsh

---
+++ sources/modules.zsh
@@ -0,0 +1,6 @@
+# git-backup module
+export PATH="$DOTFILES_PATH"/modules/git-backup/bin:$PATH
+
+# goto-folder module
+# source "$DOTFILES_PATH"/modules/goto-folder/goto.zsh
+# source "$DOTFILES_PATH"/modules/goto-folder/goto-completion.zsh
